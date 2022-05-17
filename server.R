cb0ba9a857a9ba302fc2f5af0739e77090563100
Harry_Maan <62587301+harry-maan@users.noreply.github.com>
2022-05-17 20:41:58 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,7 +1,5 @@
+install.packages("shiny")
 library(shiny)
-
-# use the below options code if you wish to increase the file input limit, in this example file input limit is increased from 5MB to 9MB
-# options(shiny.maxRequestSize = 9*1024^2)
 
 shinyServer(function(input,output) {
   
@@ -23,17 +21,6 @@ shinyServer(function(input,output) {
     if(is.null(input$file)){return ()}
     str(input$file)
   })
-  
-  ## Side bar select input widget coming through renderUI()
-  # Following code displays the select input widget with the list of file loaded by the user
-  #output$selectfile <- renderUI({
-   # if(is.null(input$file)) {return()}
-    #list(hr(), 
-     #    helpText("Select the files for which you need to see data and summary stats"),
-      #   selectInput("Select", "Select", choices=input$file$name)
-    #)
-    
-#  })
   
   ## Dataset code ##
   # This reactive output contains the dataset and display the dataset in table format
