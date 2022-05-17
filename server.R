3df10d1e7b22828b3592c42cb58d1eb8325453a2
Harry_Maan <62587301+harry-maan@users.noreply.github.com>
2022-05-17 20:07:25 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(bio3d)
+
 # use the below options code if you wish to increase the file input limit, in this example file input limit is increased from 5MB to 9MB
 # options(shiny.maxRequestSize = 9*1024^2)
 
